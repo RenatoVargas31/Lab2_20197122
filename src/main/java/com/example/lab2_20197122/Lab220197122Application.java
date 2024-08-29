48c3536f5bcc53ae8b507b55522a0f1d9e617498
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-08-28 19:16:59 -0500

    Initial commit

---
+++ src/main/java/com/example/lab2_20197122/Lab220197122Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20197122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220197122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220197122Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2_20197122/Lab220197122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20197122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220197122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
